2af7fa633dea906afc3a6a0831b8b50cb51f46f5
divyams <divyams@asb9700u-d01.csil.sfu.ca>
2021-03-08 10:04:39 -0800

    Csil edits for part 3

--- candykids.c
+++ candykids.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
+#include "bbuff.h"
+#include "stats.h"
 
 int main(int argc, char* argv[])
 {
@@ -18,31 +21,33 @@ int main(int argc, char* argv[])
         printf("Argument is not valid");
         exit(-1);
     }
-    factories = argv[1];
-    kids = argv[2];
-    seconds = argv[3];
+    factories = atoi(argv[1]);
+    kids = atoi(argv[2]);
+    seconds = atoi(argv[3]);
 
     // 2. Initialize modules 
     bbuff_init();
     stats_init(factories);
 
     // 3. Launch candy-factory threads
-    pthread_t factory_threads[factories];
-    int factory_id[factories];
+    //pthread_t factory_threads[factories];
+    //int factory_id[factories];
     for(i=0; i<factories; i++){
-        factory_id[i] = i;
+        //factory_id[i] = i;
         //pthread_create(&factory_threads[i], NULL,  ,&factory_id[i]);
 
     }
 
     // 4. Launch kid threads 
-    pthread_t kid_threads[kids];
+    //pthread_t kid_threads[kids];
     for(i=0; i<kids; i++){
         //pthread_create(&kid_threads[i], NULL,  ,NULL);
     }
 
     // 5. Wait for requested time 
-    
+    for(i=0;i<seconds;i++){
+	    sleep(1);
+    }
     // 6. Stop candy-factory threads 
     // 7. Wait until no more candy 
     // 8. Stop kid threads 

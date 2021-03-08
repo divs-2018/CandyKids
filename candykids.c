d2e5611146da6b8979d3996b63a0df9da71b9218
divyams <divyams@asb9700u-d01.csil.sfu.ca>
2021-03-08 02:41:24 -0800

    Started part 1 of main

--- candykids.c
+++ candykids.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
+    //Variables
+    int i = 0;
 
+    //1. Extract Arguments
+    for (i=1; i< argc; i++) {
+    	if(argv[i] < 0){
+		printf("Argument is not valid");
+		exit(-1);
+	}
+    }    
     return 0;
 }

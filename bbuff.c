d26d5c9acf62b2145cff6481e9b2c785107dacd9
SharanjitV101 <45416293+SharanjitV101@users.noreply.github.com>
2021-03-09 22:52:21 -0800

    Update bbuff.c

--- bbuff.c
+++ bbuff.c
@@ -4,35 +4,39 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <semaphore.h>
 
 
 void* buffer[BUFFER_SIZE];
 int in, out;
-int empty; //counting semaphore: number of empty slots
-int full; //counting semaphore: number of filled slots
-int mutex; //binary semaphore
+sem_t empty; //counting semaphore: number of empty slots
+sem_t full; //counting semaphore: number of filled slots
+sem_t mutex; //binary semaphore
 void* itemConsumed = malloc (sizeof(void*));
 
 void bbuff_init(void){
     in = 0;
     out = 0;
-    empty = BUFFER_SIZE;
-    mutex = 1;
-    full = 0; 
+    sem_init(&empty,0,1);
+    sem_init(&mutex,0,1);
+    sem_init(&full,0,1); 
 };
 
 void bbuff_blocking_insert(void *item){
     do{
          
-        wait(empty); //wait until there is at least one empty slot
-        wait(mutex); //acquire lock
+        sem_wait(&empty); //wait until there is at least one empty slot
+        sem_wait(&mutex); //acquire lock
 
         //Insert item in buffer when not full and increment counter
         buffer[in] = item;
         in = (in + 1) % BUFFER_SIZE;
 
-        signal(mutex); //release lock
-        signal(full); //increment full
+        sem_post(&mutex); //release lock
+        sem_post(&full); //increment full
       
     }while(true);
 }
@@ -41,15 +45,15 @@ void bbuff_blocking_insert(void *item){
 void* bbuff_blocking_extract(void){
     do{
          
-        wait(full); //wait until there is one full slot in the buffer
-        wait(mutex); //acquire lock
+        sem_wait(&full); //wait until there is one full slot in the buffer
+        sem_wait(&mutex); //acquire lock
 
         //Remove item when buffer not empty, remove item and decrement counter
         itemConsumed = buffer[out];
         out = (out + 1) % BUFFER_SIZE;
 
-        signal(mutex); //release lock
-        signal(empty); //increment 'empty' becuase consumer has acquired removed the item
+        sem_post(&mutex); //release lock
+        sem_post(^empty); //increment 'empty' becuase consumer has acquired removed the item
         
         //counsume the candy
 

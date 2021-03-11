2c6cb9b5b29bc2b2e36cd27b193030ea2cbac640
SharanjitV101 <45416293+SharanjitV101@users.noreply.github.com>
2021-03-11 01:44:15 -0800

    Update bbuff.c

--- bbuff.c
+++ bbuff.c
@@ -8,13 +8,18 @@
 #include <semaphore.h>
 #include <stdbool.h>
 
-
+/*
+Declare variables
+*/
 void* buffer[BUFFER_SIZE];
 sem_t empty; //counting semaphore: number of empty slots
 sem_t full; //counting semaphore: number of filled slots
 sem_t mutex; //binary semaphore
 int in, out;
 
+/*
+Initialize variables
+*/
 void bbuff_init(void){
     in = 0;
     out = 0;
@@ -23,6 +28,9 @@ void bbuff_init(void){
     sem_init(&full,SHARED_THREADS,0); 
 };
 
+/*
+Function to insert items from the buffer 
+*/
 void bbuff_blocking_insert(void *item){
     sem_wait(&empty); //wait until there is at least one empty slot
     sem_wait(&mutex); //acquire lock
@@ -30,14 +38,16 @@ void bbuff_blocking_insert(void *item){
     //Insert item in buffer when not full and increment counter
     buffer[in] = item;
     in = (in+1)%BUFFER_SIZE;
+    
     sem_post(&mutex); //release lock
     sem_post(&full); //increment full
     
 }
 
-
+/*
+Function to extract items from the buffer 
+*/
 void* bbuff_blocking_extract(void){
-
     sem_wait(&full); //wait until there is one full slot in the buffer
     sem_wait(&mutex); //acquire lock
 
@@ -46,13 +56,13 @@ void* bbuff_blocking_extract(void){
     void* item = buffer[out];
     buffer[out] = NULL;
     out = (out+1)%BUFFER_SIZE;
+    
     sem_post(&mutex); //release lock
     sem_post(&empty); //increment 'empty' becuase consumer has acquired removed the item
 
     return item; 
-
 }
 
 _Bool bbuff_is_empty(void){
-    return in == out;
+    return in == out; //Check if the buffer is empty
 };
